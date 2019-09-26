8b45008b447b118d5dcaffa83511592e31ccf843
DESKTOP-RBS8IG3\user <qqbck123@ebizville.co.kr>
2019-09-26 20:16:21 +0900

    <meta name="format-detection" content="telephone=no"> 추가

--- landing-page-m/@resource/js/list.js
+++ landing-page-m/@resource/js/list.js
@@ -50,7 +50,7 @@
 	      if (st > lastScrollTop){
 	        // downscroll code
 	        if(wentUp){
-	          header.removeClass('load-effect');
+	          header.addClass('js-hide');
 	          goUp.removeClass('js-hide');
 	        }
 	        wentUp = false;
@@ -58,7 +58,7 @@
 	      } else {
 	        // upscroll code
 	        if(wentDown){
-	          header.addClass('load-effect');
+	          header.removeClass('js-hide');
 	          goUp.addClass('js-hide');
 	        }
 	        wentUp = true;
